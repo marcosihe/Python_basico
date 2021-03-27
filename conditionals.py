923b89f627b63801ca8b470a8afc5e127462827e
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-27 19:30:58 -0300

    Se modifico el archivo para mostrar el uso del 'elif'.

--- conditionals.py
+++ conditionals.py
@@ -4,3 +4,11 @@ if edad > 18:
     print('Eres mayor de edad')
 else:
     print('Eres menor de edad')
+
+
+if edad > 20:
+    print('La edad ingresada es mayor a 20')
+elif edad < 20:
+    print('La edad ingresada es menor a 20')
+else:
+    print('La edad ingresada es 20')
