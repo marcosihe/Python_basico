78fb78324a5f0237802bafea9783d579a378a46b
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-28 00:58:59 -0300

    Se agrego el archivo loop_strings.py en el cual se trabaja con el ciclo for recorriendo un string o cadena de texto.

---
+++ loop_strings.py
@@ -0,0 +1,11 @@
+def run():
+    # name_ = input('Escriba su nombre: ')
+    # for char in name_:
+    #    print(char)
+    sentence = input('Escriba una frase: ')
+    for char_ in sentence:
+        print(char_.upper())
+
+
+if __name__ == '__main__':
+    run()
