2466d45847f014a516f03dd05403bf58e47fed81
Marcos Herrera <marcosherrera.ib@gmail.com>
2021-03-27 22:36:00 -0300

    Se trabajo sobre el ciclo while en el archivo while_loop.py.

---
+++ while_loop.py
@@ -0,0 +1,14 @@
+# para definir una constante se debe indicar su nombre todo en MAYÚCULAS
+def run():
+    LIMITE = 1000
+
+    counter = 0
+    power_2 = 2**counter
+    while power_2 < LIMITE:
+        print('2 elevado a ' + str(counter) + ' es igual a: ' + str(power_2))
+        counter = counter + 1
+        power_2 = 2**counter
+
+
+if __name__ == '__main__':
+    run()
